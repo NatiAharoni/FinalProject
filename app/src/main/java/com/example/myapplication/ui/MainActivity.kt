2fd92dd840b270fb997bf295cb48036af7403b49
Menashh <96311912+Menashh@users.noreply.github.com>
2023-06-23 11:36:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -17,10 +17,10 @@ class MainActivity : AppCompatActivity() {
         val binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController,appBarConfiguration)
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //val navController = navHostFragment.navController
+        //val appBarConfiguration = AppBarConfiguration(navController.graph)
+        //binding.toolbar.setupWithNavController(navController,appBarConfiguration)
 
     }
 }
